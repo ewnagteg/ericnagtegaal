9981a505c79a63859ac81a25eac3ed6867ce5c06
Eric Nagtegaal <ewnagteg@uwaterloo.ca>
2025-05-31 19:52:03 -0400

    set save to close

--- src/components/MessageProvider.js
+++ src/components/MessageProvider.js
@@ -20,7 +20,7 @@ export const MessageProvider = ({ children }) => {
                                setMessage("");
                             }}
                         >
-                            Save
+                            Close
                         </button>
                     </div>
                 </div>
