cf19b7c2c4a9d827fee0c8df70f2c2ff2ae6e1c7
Eric Nagtegaal <ewnagteg@uwaterloo.ca>
2025-07-13 20:55:11 -0400

    update constants

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const LOCK_DATE = new Date('2025-06-07T12:00:00Z');
-export const BUDGET = 1800;
+export const LOCK_DATE = new Date('2025-07-16T12:00:00Z');
+export const BUDGET = 40;
 export const DEV = false; // todo: use env var, this is just for testing
