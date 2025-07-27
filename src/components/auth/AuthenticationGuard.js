ae0d0ede929cfe5300c1e6b5bbfbfdf4dffd1b64
Eric Nagtegaal <ewnagteg@uwaterloo.ca>
2025-07-27 16:38:27 -0400

    fix redirect colour

--- src/components/auth/AuthenticationGuard.js
+++ src/components/auth/AuthenticationGuard.js
@@ -7,7 +7,7 @@ export const AuthenticationGuard = ({ component }) => {
   const Component = withAuthenticationRequired(component, {
     onRedirecting: () => (
       <div className="fixed inset-0 bg-gray-900 page-layout flex items-center justify-center">
-        <p>Redirect...</p>
+        <p className="text-white text-xl font-bold">Redirect...</p>
       </div>
     ),
   });
